521920eee24d2b8d80be817032d4a7c8730e8cd0
FaithFromund <41239690+FaithFromund@users.noreply.github.com>
2018-07-14 11:21:05 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,14 @@
-{
-  "ClientId": 453038960726507530,
-  "Token": "NDUzMDM4OTYwNzI2NTA3NTMw.DivCOg.EZ5QuzEMpvgITKM-xozJak485q8",
-  "OwnerIds": [
-    371350015630901262,376184330038542337
-    
-    
-  ],
-  "LoLApiKey": "",
-  "GoogleApiKey": "AIzaSyBTZ0AD7Rc42sl3bC9aQfJy9qu0rFJlHwY",
-  "MashapeKey": "",
-  "OsuApiKey": "",
-  "SoundCloudClientId": "",
-  "CleverbotApiKey": "",
-  "CarbonKey": "",
-  "Db": {
-    "Type": "sqlite",
-    "ConnectionString": "Data Source=data/NadekoBot.db"
-  },
-  "TotalShards": 1,
-  "PatreonAccessToken": "",
-  "PatreonCampaignId": "334038",
-  "RestartCommand": null,
-  "ShardRunCommand": "",
-  "ShardRunArguments": "",
-  "ShardRunPort": null,
-  "MiningProxyUrl": null,
-  "MiningProxyCreds": null,
-  "BotListToken": null,
-  "TwitchClientId": null,
-  "VotesToken": null,
-  "VotesUrl": null
-}
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+// THIS MUST BE THIS WAY
+client.login(process.env.BOT_TOKEN);
