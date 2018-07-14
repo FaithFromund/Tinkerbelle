4fa270bc935a4cb85ee3a917a01674de5d15d401
FaithFromund <41239690+FaithFromund@users.noreply.github.com>
2018-07-14 11:04:09 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,33 @@
+{
+  "ClientId": 453038960726507530,
+  "Token": "NDUzMDM4OTYwNzI2NTA3NTMw.DivCOg.EZ5QuzEMpvgITKM-xozJak485q8",
+  "OwnerIds": [
+    371350015630901262,376184330038542337
+    
+    
+  ],
+  "LoLApiKey": "",
+  "GoogleApiKey": "AIzaSyBTZ0AD7Rc42sl3bC9aQfJy9qu0rFJlHwY",
+  "MashapeKey": "",
+  "OsuApiKey": "",
+  "SoundCloudClientId": "",
+  "CleverbotApiKey": "",
+  "CarbonKey": "",
+  "Db": {
+    "Type": "sqlite",
+    "ConnectionString": "Data Source=data/NadekoBot.db"
+  },
+  "TotalShards": 1,
+  "PatreonAccessToken": "",
+  "PatreonCampaignId": "334038",
+  "RestartCommand": null,
+  "ShardRunCommand": "",
+  "ShardRunArguments": "",
+  "ShardRunPort": null,
+  "MiningProxyUrl": null,
+  "MiningProxyCreds": null,
+  "BotListToken": null,
+  "TwitchClientId": null,
+  "VotesToken": null,
+  "VotesUrl": null
+}
